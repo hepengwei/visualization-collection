8631e386f45ac97e756f37ef214f3076ca33571a
hepengwei <1007941801@qq.com>
2022-11-14 16:23:18 +0800

    修改快速切换文字组件

--- src/pages/canvas/CanvasComingSoon.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ComingSoon from "@/components/ComingSoon";
-
-const CanvasComingSoon = () => {
-  return <ComingSoon />;
-};
-
-export default CanvasComingSoon;
--- src/pages/html/HtmlComingSoon.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ComingSoon from "@/components/ComingSoon";
-
-const HtmlComingSoon = () => {
-  return <ComingSoon />;
-};
-
-export default HtmlComingSoon;
