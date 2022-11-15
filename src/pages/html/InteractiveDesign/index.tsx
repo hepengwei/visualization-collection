bc1156b2d587098870fa558bf745d784e9103fc1
hepengwei <1007941801@qq.com>
2022-11-15 11:16:02 +0800

    新增页面鼠标Hover交互效果

--- src/pages/html/InteractiveDesign/index.tsx
+++ src/pages/html/InteractiveDesign/index.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import MagnifyingGlass from "./components/MagnifyingGlass";
 import CropImage from "./components/CropImage";
 import DragShopping from "./components/DragShopping";
+import MouseHover from "./components/MouseHover";
 import styles from "./index.module.less";
 
 const InteractiveDesign = () => {
@@ -18,6 +19,9 @@ const InteractiveDesign = () => {
       </div>
       <div className={styles.box}>
         <DragShopping />
+      </div>
+      <div className={styles.box}>
+        <MouseHover />
       </div>
     </div>
   );
