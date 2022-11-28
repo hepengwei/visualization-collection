37f759b5cc36e9dff21f24078e3bb722553065f5
hepengwei <1007941801@qq.com>
2022-11-28 13:55:49 +0800

    新增倒影效果

---
+++ src/pages/css/RichDynamicEffect/components/BoxReflect/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./index.module.less";
+
+const BoxReflect = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.btn1}>Reflect</div>
+        <div className={styles.btn2}>Reflect</div>
+      </div>
+    </div>
+  );
+};
+
+export default BoxReflect;
