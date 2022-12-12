02a6c4f3372bc05a1b986e7dbeab3ef01d554936
hepengwei <1007941801@qq.com>
2022-12-12 18:06:37 +0800

    新增人物飘浮动画

---
+++ src/pages/html/InteractiveDesign/components/PeopleAnimation/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Animation from "./Animation";
+import styles from "./index.module.less";
+
+const PeopleAnimation = () => {
+  return (
+    <div className={styles.container}>
+      <Animation />
+    </div>
+  );
+};
+
+export default PeopleAnimation;
