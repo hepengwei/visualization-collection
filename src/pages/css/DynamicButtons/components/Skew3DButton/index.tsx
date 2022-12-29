2e823e2553d09fc0e04ba930892b4fc371f66171
hepengwei <1007941801@qq.com>
2022-12-29 19:23:15 +0800

    新增3D斜切按钮

---
+++ src/pages/css/DynamicButtons/components/Skew3DButton/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+
+const Skew3DButton = () => {
+  return (
+    <div className={styles.container}>
+      <button>Button</button>
+    </div>
+  );
+};
+
+export default Skew3DButton;
