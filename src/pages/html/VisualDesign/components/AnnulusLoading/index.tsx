1d75e66fd0572eb9350b68c25b7704e34bee5043
hepengwei <1007941801@qq.com>
2023-01-04 11:00:50 +0800

    新增七彩圆环Loading动画

---
+++ src/pages/html/VisualDesign/components/AnnulusLoading/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+const AnnulusLoading = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.item1} />
+        <div className={styles.item2} />
+        <div className={styles.item3} />
+        <div className={styles.item4} />
+        <div className={styles.item5} />
+        <div className={styles.item6} />
+        <div className={styles.item7} />
+      </div>
+    </div>
+  );
+};
+
+export default AnnulusLoading;
