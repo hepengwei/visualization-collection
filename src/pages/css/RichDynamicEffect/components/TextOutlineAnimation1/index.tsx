5c3d9522ace4175ae7f14983ed48c3d59512ce21
river <1007941801@qq.com>
2023-02-04 17:44:47 +0800

    新增文字轮廓描边显示动画

---
+++ src/pages/css/RichDynamicEffect/components/TextOutlineAnimation1/index.tsx
@@ -0,0 +1,35 @@
+import React, { useRef } from "react";
+import Text from "images/text.svg";
+import styles from "./index.module.scss";
+
+const TextOutlineAnimation1 = () => {
+  const conetentRef = useRef<HTMLDivElement>(null);
+
+  //   // 获取每个SVG文字的path路径长度
+  //   const getSvgTextLength = () => {
+  //     if (conetentRef.current) {
+  //       const svgEleList = conetentRef.current.children;
+  //       const path: HTMLCollection = svgEleList[0].children;
+  //       for (let i = 0; i < path.length; i++) {
+  //         const pathEle = path[i] as SVGPathElement;
+  //         const pathLength = Math.round(pathEle.getTotalLength());
+  //         console.log(
+  //           `第${i}个字长度为：${(path[i] as SVGPathElement).getTotalLength()}`
+  //         );
+  //       }
+  //     }
+  //   };
+  //   useEffect(() => {
+  //     getSvgTextLength();
+  //   }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content} ref={conetentRef}>
+        <Text />
+      </div>
+    </div>
+  );
+};
+
+export default TextOutlineAnimation1;
