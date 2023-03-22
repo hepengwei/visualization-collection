62d017742eb5ee17590a5b9a4e57d53233851e9d
hepengwei <1007941801@qq.com>
2023-03-22 13:17:44 +0800

    新增六边形网格交互效果2

---
+++ src/pages/html/InteractiveDesign/components/HexagonalMesh2/index.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef } from "react";
+import styles from "./index.module.scss";
+
+const list = new Array(800).fill(0);
+
+const HexagonalMesh2 = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const onMouseMove = (e: React.MouseEvent) => {
+    if (containerRef.current) {
+      const { left, top } = containerRef.current.getBoundingClientRect();
+      const x = e.pageX - left;
+      const y = e.pageY - top;
+      window.requestAnimationFrame(function () {
+        if (containerRef.current) {
+          containerRef.current.style.setProperty("--xPos", `${x}px`);
+          containerRef.current.style.setProperty("--yPos", `${y}px`);
+        }
+      });
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (containerRef.current) {
+      const { width, height } = containerRef.current.getBoundingClientRect();
+      const x = width / 2;
+      const y = height / 2;
+      containerRef.current.style.setProperty("--xPos", `${x}px`);
+      containerRef.current.style.setProperty("--yPos", `${y}px`);
+    }
+  };
+
+  useEffect(() => {
+    onMouseLeave();
+  }, []);
+
+  return (
+    <div
+      className={styles.container}
+      onMouseMove={onMouseMove}
+      onMouseLeave={onMouseLeave}
+      ref={containerRef}
+    >
+      {list.map((item: number, index: number) => (
+        <div key={index} className={styles.item} />
+      ))}
+    </div>
+  );
+};
+
+export default HexagonalMesh2;
