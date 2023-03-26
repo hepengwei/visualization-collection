588d0f6eb62804acf5ea5b5a7eb4492367c422a0
river <1007941801@qq.com>
2023-03-26 19:01:05 +0800

    去掉个人信息

---
+++ src/pages/html/InteractiveDesign/components/WipeSwitchover/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+const WipeSwitchover = () => {
+  return <div className={styles.container}>000</div>;
+};
+
+export default WipeSwitchover;
