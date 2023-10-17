5740def51b7da4ade78623c4b9555faa800d80f2
River <1007941801@qq.com>
2023-10-17 14:38:26 +0800

    3D地图增加鼠标交互，高亮显示

--- src/components/LargeScreenBorder/Border1.tsx
+++ src/components/LargeScreenBorder/Border1.tsx
@@ -1,13 +1,14 @@
 import React, { ReactNode, useEffect, useMemo, useRef } from "react";
 import { useIntl } from "react-intl";
 import { getTextWidth } from "utils/util";
+import { useGlobalContext } from "hooks/useGlobalContext";
 import styles from "./index.module.scss";
 
 interface BorderPorps {
   title?: string;
   lineWidth?: number; // 边框线宽度
   lineColor?: string; // 边框线颜色
-  highlightColor?: string; // 个别高亮颜色
+  highlightColor?: string; // 高亮时颜色
   style?: Record<string, any>;
   children?: ReactNode;
 }
@@ -17,19 +18,28 @@ const leftToplineWidth = 8; // 左上角三个横线各自的宽度
 const leftToplineMargin = 4; // 左上角三个横线中间的间距
 const titleMarginTop = 6; // 标题距离上边边框的距离
 const titleMarginBottom = 20; // 标题距离上边边框的距离
+const bgItemWidth = 10; // 背景图案每个小格的宽度
+const bgColor1 = "#3D5454"; // 背景图案的小格的颜色1
+const bgColor2 = "#445A5A"; // 背景图案的小格的颜色2
+const arrowWidth = 10; // 标题左右各箭头的宽度
+const arrowLineWidth = 4; // 标题左右各箭头线的粗细
+const arrowMargin = 6; // 标题左右各箭头的间距
+const arrowMarginTitle = 16; // 标题左右箭头与标题的间距
 
 const Border1 = (props: BorderPorps) => {
   const {
     title = "",
     lineWidth = 1,
-    lineColor = "#005A5A",
-    highlightColor = "#41B1B4",
+    lineColor = "#005A5A", // 边框线默认颜色
+    highlightColor = "#41B1B4", // 高亮时默认颜色
     style = {},
   } = props;
   const intl = useIntl();
+  const { menuWidth } = useGlobalContext();
   const containerRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
+  const isHover = useRef<boolean>(false);
 
   const finalStyle = useMemo(() => {
     const defaultStyle = {
@@ -40,11 +50,11 @@ const Border1 = (props: BorderPorps) => {
     return { ...defaultStyle, ...style };
   }, [style]);
 
-  const draw = () => {
-    if (canvasRef.current && ctxRef.current) {
+  // 绘制边框路径
+  const drawBorderPath = (drawLineWidth: number) => {
+    const ctx = ctxRef.current;
+    if (canvasRef.current && ctx) {
       const { width, height } = canvasRef.current;
-      const ctx = ctxRef.current;
-      // 画边框
       let x = 1;
       ctx.beginPath();
       ctx.moveTo(x, leftTopHeight);
@@ -56,20 +66,67 @@ const Border1 = (props: BorderPorps) => {
       ctx.lineTo(x, leftTopHeight);
       x = width - 2 * leftTopHeight;
       ctx.lineTo(x, leftTopHeight);
-      x = width;
+      x = width - drawLineWidth;
       ctx.lineTo(x, 3 * leftTopHeight);
-      ctx.lineTo(x, height);
+      ctx.lineTo(x, height - drawLineWidth);
       x = 2 * leftTopHeight;
-      ctx.lineTo(x, height);
+      ctx.lineTo(x, height - drawLineWidth);
       x = 1;
       ctx.lineTo(x, height - 2 * leftTopHeight);
       ctx.lineTo(x, leftTopHeight);
       ctx.closePath();
-      ctx.lineWidth = lineWidth;
-      ctx.strokeStyle = lineColor;
+      ctx.lineWidth = drawLineWidth;
+    }
+  };
+
+  const draw = () => {
+    if (canvasRef.current && ctxRef.current) {
+      const { width, height } = canvasRef.current;
+      const ctx = ctxRef.current;
+      ctx.clearRect(0, 0, width, height);
+      ctx.globalCompositeOperation = "source-over";
+      let drawLineWidth = lineWidth;
+      // hover时绘制背景图案
+      if (isHover.current) {
+        // 线加粗
+        drawLineWidth += 1;
+        drawBorderPath(drawLineWidth);
+        ctx.fillStyle = isHover.current ? highlightColor : lineColor;
+        ctx.fill();
+        ctx.globalCompositeOperation = "source-atop";
+        let x = 0;
+        let y = 0;
+        while (y <= height && x <= width) {
+          const nextX = x + bgItemWidth;
+          const indexX = Math.floor(x / bgItemWidth);
+          const indexY = Math.floor(y / bgItemWidth);
+
+          if (
+            (indexX % 2 === 0 && indexY % 2 === 0) ||
+            (indexX % 2 !== 0 && indexY % 2 !== 0)
+          ) {
+            ctx.fillStyle = bgColor1;
+          } else {
+            ctx.fillStyle = bgColor2;
+          }
+          if (nextX > width) {
+            ctx.fillRect(x, y, width - x, bgItemWidth);
+            y += bgItemWidth;
+            x = 0;
+          } else {
+            ctx.fillRect(x, y, bgItemWidth, bgItemWidth);
+            x += bgItemWidth;
+          }
+        }
+      }
+      ctx.globalCompositeOperation = "source-over";
+      // 画边框
+      drawBorderPath(drawLineWidth);
+      let x = 1;
+      ctx.strokeStyle = isHover.current ? highlightColor : lineColor;
       ctx.stroke();
       // 修改画笔粗细和颜色
-      ctx.lineWidth = lineWidth + 1;
+      ctx.lineWidth = drawLineWidth + 1;
       ctx.strokeStyle = highlightColor;
       // 画左上角三个横线
       ctx.beginPath();
@@ -97,31 +154,100 @@ const Border1 = (props: BorderPorps) => {
       ctx.stroke();
       // 画右下角直角
       ctx.beginPath();
-      x = width;
+      x = width - drawLineWidth;
       ctx.moveTo(x, height - 2 * leftTopHeight);
       ctx.lineTo(x, height);
       ctx.closePath();
       ctx.stroke();
       ctx.beginPath();
-      ctx.moveTo(x, height);
+      ctx.moveTo(x, height - drawLineWidth);
       x -= 2 * leftTopHeight;
-      ctx.lineTo(x, height);
+      ctx.lineTo(x, height - drawLineWidth);
       ctx.closePath();
       ctx.stroke();
 
-      // 绘制标题
-      const titleWidth = getTextWidth(
-        title,
-        Number.parseInt(finalStyle.fontSize),
-        finalStyle.fontWeight
-      );
-      ctx.font = `${finalStyle.fontWeight} ${finalStyle.fontSize} SourceHanSansCN-Bold`;
-      ctx.fillStyle = finalStyle.color;
-      ctx.fillText(
-        title,
-        (width - titleWidth) / 2,
-        leftTopHeight + titleMarginTop + Number.parseInt(finalStyle.fontSize)
-      );
+      if (title) {
+        // 绘制标题
+        const titleWidth = getTextWidth(
+          title,
+          Number.parseInt(finalStyle.fontSize),
+          finalStyle.fontWeight
+        );
+        ctx.font = `${finalStyle.fontWeight} ${finalStyle.fontSize} SourceHanSansCN-Bold`;
+        ctx.fillStyle = finalStyle.color;
+        ctx.fillText(
+          title,
+          (width - titleWidth) / 2,
+          leftTopHeight + titleMarginTop + Number.parseInt(finalStyle.fontSize)
+        );
+        let x =
+          (width - titleWidth) / 2 -
+          (arrowMarginTitle + 2 * arrowWidth + arrowMargin);
+        const y =
+          leftTopHeight +
+          titleMarginTop +
+          Number.parseInt(finalStyle.fontSize) / 2 +
+          2;
+        // 绘制标题左边箭头图案
+        ctx.lineWidth = arrowLineWidth;
+        ctx.lineJoin = "miter";
+        ctx.beginPath();
+        ctx.moveTo(x, leftTopHeight + titleMarginTop + 4);
+        ctx.lineTo(x + arrowWidth, y);
+        ctx.lineTo(
+          x,
+          leftTopHeight + titleMarginTop + Number.parseInt(finalStyle.fontSize)
+        );
+        ctx.strokeStyle = isHover.current ? finalStyle.color : lineColor;
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(
+          x + arrowWidth + arrowMargin,
+          leftTopHeight + titleMarginTop + 4
+        );
+        ctx.lineTo(x + 2 * arrowWidth + arrowMargin, y);
+        ctx.lineTo(
+          x + arrowWidth + arrowMargin,
+          leftTopHeight + titleMarginTop + Number.parseInt(finalStyle.fontSize)
+        );
+        ctx.strokeStyle = finalStyle.color;
+        ctx.stroke();
+        // 绘制标题右边箭头图案
+        x = (width + titleWidth) / 2 + arrowMarginTitle;
+        ctx.beginPath();
+        ctx.moveTo(x + arrowWidth, leftTopHeight + titleMarginTop + 4);
+        ctx.lineTo(
+          x,
+          leftTopHeight +
+            titleMarginTop +
+            Number.parseInt(finalStyle.fontSize) / 2 +
+            2
+        );
+        ctx.lineTo(
+          x + arrowWidth,
+          leftTopHeight + titleMarginTop + Number.parseInt(finalStyle.fontSize)
+        );
+        ctx.strokeStyle = finalStyle.color;
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(
+          x + 2 * arrowWidth + arrowMargin,
+          leftTopHeight + titleMarginTop + 4
+        );
+        ctx.lineTo(
+          x + arrowWidth + arrowMargin,
+          leftTopHeight +
+            titleMarginTop +
+            Number.parseInt(finalStyle.fontSize) / 2 +
+            2
+        );
+        ctx.lineTo(
+          x + 2 * arrowWidth + arrowMargin,
+          leftTopHeight + titleMarginTop + Number.parseInt(finalStyle.fontSize)
+        );
+        ctx.strokeStyle = isHover.current ? finalStyle.color : lineColor;
+        ctx.stroke();
+      }
     }
   };
 
@@ -133,10 +259,26 @@ const Border1 = (props: BorderPorps) => {
       ctxRef.current = canvasRef.current.getContext("2d");
       draw();
     }
-  }, [title]);
+  }, [title, menuWidth]);
+
+  const onMouseEnter = () => {
+    isHover.current = true;
+    draw();
+  };
+
+  const onMouseLeave = () => {
+    isHover.current = false;
+    draw();
+  };
 
   return (
-    <div className={styles.container} style={finalStyle} ref={containerRef}>
+    <div
+      className={styles.container}
+      style={finalStyle}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      ref={containerRef}
+    >
       <canvas ref={canvasRef}>
         {intl.formatMessage({ id: "common.browserTooLow" })}
       </canvas>
