c477260cc257f99324fbe6d270a58816104b86e7
v-hepengwei1 <v-hepengwei1@xiaomi.com>
2026-08-24 20:27:07 +0800

    feat: 房屋展示模块所有玻璃窗增加铝合金边框

--- src/pages/threejs/HouseDisplay/goods/addHouseStructure.ts
+++ src/pages/threejs/HouseDisplay/goods/addHouseStructure.ts
@@ -20,6 +20,7 @@ import {
   InstancedMesh,
   Object3D,
   DynamicDrawUsage,
+  Group,
 } from "three";
 import type { AssetManager } from "hooks/threejs/useInitialize";
 // 导入大理石贴图
@@ -31,105 +32,109 @@ import marbleHeightImg from "images/threejs/marbleFloor/marble_height.png";
 // 墙体参数
 export const wallHeight = 4; // 墙体高度
 const beamHeight = 0.5; // 门框上方横梁的墙体高度
-const w = 0.3; // 墙体厚度
+const wallDepth = 0.3; // 墙体厚度
 const wallColor = 0xf4f3ef; // 珍珠白乳胶漆颜色
 const wallLabelSize = 1.5; // 墙体标签的大小
 const wallLabelColor = "#FFFF00"; // 墙体标签的颜色
+const glassDepth = 0.1; // 玻璃厚度
+const aluminiumAlloyFrameHeight = 0.1; // 玻璃铝合金包边高度
+const aluminiumAlloyFrameDepth = 0.12; // 玻璃铝合金包边厚度
+
 // 所有墙体的尺寸和位置
 const wallList = [
-  [14.4, wallHeight, w, -10.4, wallHeight / 2, -16],
-  [1.4, 0.5, w, -0.8, 3.75, -16],
-  [4.8, 1.5, w, -0.8, 0.75, -16],
-  [2.3, wallHeight, w, -2.65, wallHeight / 2, -16],
-  [1.64, wallHeight, w, 0.72, wallHeight / 2, -16],
-  [1.4, 0.5, w, 3.8, 3.75, -16],
-  [2.9, 1.5, w, 3.05, 0.75, -16],
-  [1.7, wallHeight, w, 2.25, wallHeight / 2, -16],
-  [w, wallHeight, 0.8, 17.6, wallHeight / 2, -10.8],
-  [13.2, wallHeight, w, 11, wallHeight / 2, -16],
+  [14.4, wallHeight, wallDepth, -10.4, wallHeight / 2, -16],
+  [1.4, 0.5, wallDepth, -0.8, 3.75, -16],
+  [4.8, 1.5, wallDepth, -0.8, 0.75, -16],
+  [2.31, wallHeight, wallDepth, -2.65, wallHeight / 2, -16],
+  [1.64, wallHeight, wallDepth, 0.72, wallHeight / 2, -16],
+  [1.4, 0.5, wallDepth, 3.8, 3.75, -16],
+  [2.9, 1.5, wallDepth, 3.05, 0.75, -16],
+  [1.71, wallHeight, wallDepth, 2.25, wallHeight / 2, -16],
+  [wallDepth, wallHeight, 0.8, 17.6, wallHeight / 2, -10.8],
+  [13.2, wallHeight, wallDepth, 11.1, wallHeight / 2, -16],
   // #11
-  [w, 0.5, 4, 17.6, 3.75, -13.2],
-  [w, 1.5, 4, 17.6, 0.75, -13.2],
-  [w, wallHeight, 0.8, 17.6, wallHeight / 2, -15.6],
-  [w, wallHeight, 0.8, 17.6, wallHeight / 2, -10.8],
-  [w, wallHeight, 4, 17.6, wallHeight / 2, -8.4],
-  [w, 0.5, 7.12, 17.6, 3.75, -1.92],
-  [w, 0.5, 7.12, 17.6, 0.25, -1.92],
-  [w, wallHeight, 0.92, 17.6, wallHeight / 2, -5.94],
-  [w, wallHeight, 0.92, 17.6, wallHeight / 2, 1.8],
-  [w, 0.5, 1.4, 17.6, 3.75, 5.09],
+  [wallDepth, 0.5, 4, 17.6, 3.75, -13.2],
+  [wallDepth, 1.5, 4, 17.6, 0.75, -13.2],
+  [wallDepth, wallHeight, 0.8, 17.6, wallHeight / 2, -15.6],
+  [wallDepth, wallHeight, 0.8, 17.6, wallHeight / 2, -10.8],
+  [wallDepth, wallHeight, 4, 17.6, wallHeight / 2, -8.4],
+  [wallDepth, 0.5, 7.12, 17.6, 3.75, -1.92],
+  [wallDepth, 0.5, 7.12, 17.6, 0.25, -1.92],
+  [wallDepth, wallHeight, 0.92, 17.6, wallHeight / 2, -5.94],
+  [wallDepth, wallHeight, 0.92, 17.6, wallHeight / 2, 1.8],
+  [wallDepth, 0.5, 1.4, 17.6, 3.75, 5.09],
   // #21
-  [w, 1.5, 1.4, 17.6, 0.75, 5.09],
-  [w, wallHeight, 2.2, 17.6, wallHeight / 2, 3.3],
-  [w, wallHeight, 2.94, 17.6, wallHeight / 2, 7.25],
-  [w, wallHeight, 6.4, -6.4, wallHeight / 2, 12.8],
-  [6.7, wallHeight, w, -3.2, wallHeight / 2, 15.92],
-  [w, wallHeight, 5.2, -2.56, wallHeight / 2, 7],
-  [11.4, wallHeight, w, 12.05, wallHeight / 2, 8.8],
-  [w, 0.5, 5.6, -17.6, 3.75, -12.4],
-  [w, 1.5, 5.6, -17.6, 0.75, -12.4],
-  [w, wallHeight, 0.8, -17.6, wallHeight / 2, -15.6],
+  [wallDepth, 1.5, 1.4, 17.6, 0.75, 5.09],
+  [wallDepth, wallHeight, 2.2, 17.6, wallHeight / 2, 3.3],
+  [wallDepth, wallHeight, 2.94, 17.6, wallHeight / 2, 7.25],
+  [wallDepth, wallHeight, 6.4, -6.4, wallHeight / 2, 12.8],
+  [6.7, wallHeight, wallDepth, -3.2, wallHeight / 2, 15.92],
+  [wallDepth, wallHeight, 5.2, -2.56, wallHeight / 2, 7],
+  [11.4, wallHeight, wallDepth, 12.05, wallHeight / 2, 8.8],
+  [wallDepth, 0.5, 5.6, -17.6, 3.75, -12.4],
+  [wallDepth, 1.5, 5.6, -17.6, 0.75, -12.4],
+  [wallDepth, wallHeight, 0.8, -17.6, wallHeight / 2, -15.6],
   // #31
-  [w, wallHeight, 0.8, -17.6, wallHeight / 2, -9.2],
-  [w, wallHeight, 4, -17.6, wallHeight / 2, -6.8],
-  [w, 0.5, 12.8, -17.6, 3.75, 3.2],
-  [w, 0.5, 12.8, -17.6, 0.25, 3.2],
-  [w, wallHeight, 1.6, -17.6, wallHeight / 2, -4],
-  [w, wallHeight, 1.6, -17.6, wallHeight / 2, 10.4],
-  [14.4, wallHeight, w, -8.4, wallHeight / 2, -6.4],
-  [w, wallHeight, 4.2, -3.25, wallHeight / 2, -13.7],
-  [w, wallHeight, 6.4, 1.6, wallHeight / 2, -12.8],
-  [w, wallHeight, 7.34, 6, wallHeight / 2, -12.39],
+  [wallDepth, wallHeight, 0.8, -17.6, wallHeight / 2, -9.2],
+  [wallDepth, wallHeight, 4, -17.6, wallHeight / 2, -6.8],
+  [wallDepth, 0.5, 12.8, -17.6, 3.75, 3.2],
+  [wallDepth, 0.5, 12.8, -17.6, 0.25, 3.2],
+  [wallDepth, wallHeight, 1.6, -17.6, wallHeight / 2, -4],
+  [wallDepth, wallHeight, 1.6, -17.6, wallHeight / 2, 10.4],
+  [14.4, wallHeight, wallDepth, -8.4, wallHeight / 2, -6.4],
+  [wallDepth, wallHeight, 4.2, -3.25, wallHeight / 2, -13.7],
+  [wallDepth, wallHeight, 6.4, 1.6, wallHeight / 2, -12.8],
+  [wallDepth, wallHeight, 7.34, 6, wallHeight / 2, -12.39],
   // #41
-  [8.9, wallHeight, w, -9.4, wallHeight / 2, 4.4],
-  [w, wallHeight, 2.1, -15.6, wallHeight / 2, 12.3],
-  [2.3, wallHeight, w, -16.6, wallHeight / 2, 11.3],
-  [6.7, wallHeight, w, -3.2, wallHeight / 2, 9.6],
-  [w, wallHeight, 6.4, 0, wallHeight / 2, 12.8],
-  [2.6, wallHeight, w, 16.4, wallHeight / 2, 2.3],
-  [w, wallHeight, 3, 4.8, wallHeight / 2, 3.9],
-  [w, wallHeight, 3.4, 6.4, wallHeight / 2, 7.3],
-  [3, wallHeight, w, 5.05, wallHeight / 2, 5.5],
-  [7.4, wallHeight, w, 0.3, wallHeight / 2, -9.6],
+  [8.9, wallHeight, wallDepth, -9.4, wallHeight / 2, 4.4],
+  [wallDepth, wallHeight, 2.1, -15.6, wallHeight / 2, 12.3],
+  [2.3, wallHeight, wallDepth, -16.6, wallHeight / 2, 11.3],
+  [6.7, wallHeight, wallDepth, -3.2, wallHeight / 2, 9.6],
+  [wallDepth, wallHeight, 6.4, 0, wallHeight / 2, 12.8],
+  [2.6, wallHeight, wallDepth, 16.4, wallHeight / 2, 2.3],
+  [wallDepth, wallHeight, 3, 4.8, wallHeight / 2, 3.9],
+  [wallDepth, wallHeight, 3.4, 6.4, wallHeight / 2, 7.3],
+  [3, wallHeight, wallDepth, 5.05, wallHeight / 2, 5.5],
+  [7.4, wallHeight, wallDepth, 0.3, wallHeight / 2, -9.6],
   // #51
-  [11.6, wallHeight, w, 11.65, wallHeight / 2, -6.4],
-  [2, wallHeight, w, -16.6, wallHeight / 2, -8.8],
-  [2, wallHeight, w, -16.6, wallHeight / 2, -4.8],
-  [w, wallHeight, 4.3, -15.6, wallHeight / 2, -6.8],
-  [w, wallHeight, 4, 15.6, wallHeight / 2, -8.4],
-  [2.1, wallHeight, w, 16.5, wallHeight / 2, -10.4],
-  [w, wallHeight, 2, -13.2, wallHeight / 2, -5.4],
-  [w, wallHeight, 1.5, -13.2, wallHeight / 2, 3.65],
-  [2.4, wallHeight, w, 5.85, wallHeight / 2, 2.3],
-  [4.1, wallHeight, w, -1.1, wallHeight / 2, 4.4],
+  [11.6, wallHeight, wallDepth, 11.65, wallHeight / 2, -6.4],
+  [2, wallHeight, wallDepth, -16.6, wallHeight / 2, -8.8],
+  [2, wallHeight, wallDepth, -16.6, wallHeight / 2, -4.8],
+  [wallDepth, wallHeight, 4.3, -15.6, wallHeight / 2, -6.8],
+  [wallDepth, wallHeight, 4, 15.6, wallHeight / 2, -8.4],
+  [2.1, wallHeight, wallDepth, 16.5, wallHeight / 2, -10.4],
+  [wallDepth, wallHeight, 2, -13.2, wallHeight / 2, -5.4],
+  [wallDepth, wallHeight, 1.5, -13.2, wallHeight / 2, 3.65],
+  [2.4, wallHeight, wallDepth, 5.85, wallHeight / 2, 2.3],
+  [4.1, wallHeight, wallDepth, -1.1, wallHeight / 2, 4.4],
   // #61
-  [w, wallHeight, 1.8, -14, wallHeight / 2, 12.3],
-  [w, wallHeight, 1.5, 1, wallHeight / 2, 5],
-  [9.2, wallHeight, w, -11, wallHeight / 2, 13.2],
-  [w, wallHeight, 1.5, -14, wallHeight / 2, 5],
-  [w, beamHeight, 8, -13.2, wallHeight - beamHeight / 2, -0.7],
-  [8.2, beamHeight, w, 11, wallHeight - beamHeight / 2, 2.3],
-  [2.4, beamHeight, w, 2.35, wallHeight - beamHeight / 2, 5.5],
-  [w, beamHeight, 2, -1.35, wallHeight - beamHeight / 2, -7.8],
-  [w, beamHeight, 2, 6, wallHeight - beamHeight / 2, -7.8],
-  [w, beamHeight, 1.9, -3.25, wallHeight - beamHeight / 2, -10.68],
+  [wallDepth, wallHeight, 1.8, -14, wallHeight / 2, 12.3],
+  [wallDepth, wallHeight, 1.5, 1, wallHeight / 2, 5],
+  [9.2, wallHeight, wallDepth, -11, wallHeight / 2, 13.2],
+  [wallDepth, wallHeight, 1.5, -14, wallHeight / 2, 5],
+  [wallDepth, beamHeight, 8, -13.2, wallHeight - beamHeight / 2, -0.7],
+  [8.2, beamHeight, wallDepth, 11, wallHeight - beamHeight / 2, 2.3],
+  [2.4, beamHeight, wallDepth, 2.35, wallHeight - beamHeight / 2, 5.5],
+  [wallDepth, beamHeight, 2, -1.35, wallHeight - beamHeight / 2, -7.8],
+  [wallDepth, beamHeight, 2, 6, wallHeight - beamHeight / 2, -7.8],
+  [wallDepth, beamHeight, 1.9, -3.25, wallHeight - beamHeight / 2, -10.68],
   // #71
-  [2.1, beamHeight, w, 4.85, wallHeight - beamHeight / 2, -9.6],
-  [w, beamHeight, 6, -14, wallHeight - beamHeight / 2, 8.5],
-  [1.8, beamHeight, w, -4.05, wallHeight - beamHeight / 2, 4.4],
-  [w, wallHeight, 0.45, -1.35, wallHeight / 2, -6.72],
-  [w, wallHeight, 0.75, -1.35, wallHeight / 2, -9.07],
-  [w, wallHeight, 0.42, 6, wallHeight / 2, -6.73],
+  [2.1, beamHeight, wallDepth, 4.85, wallHeight - beamHeight / 2, -9.6],
+  [wallDepth, beamHeight, 6, -14, wallHeight - beamHeight / 2, 8.5],
+  [1.8, beamHeight, wallDepth, -4.05, wallHeight - beamHeight / 2, 4.4],
+  [wallDepth, wallHeight, 0.45, -1.35, wallHeight / 2, -6.72],
+  [wallDepth, wallHeight, 0.75, -1.35, wallHeight / 2, -9.07],
+  [wallDepth, wallHeight, 0.42, 6, wallHeight / 2, -6.73],
 ];
 // 所有玻璃窗的尺寸和位置
 const glassList = [
-  [1.4, 2.5, 0.1, -0.8, 2.5, -16],
-  [1.4, 2.5, 0.1, 3.8, 2.5, -16],
-  [0.1, 2.5, 4, 17.6, 2.5, -13.2],
-  [0.1, 3.5, 7.12, 17.6, 2, -1.92],
-  [0.1, 2, 1.4, 17.6, 2.5, 5.09],
-  [0.1, 2.5, 5.6, -17.6, 2.5, -12.4],
-  [0.1, 3.5, 12.8, -17.6, 2, 3.2],
+  [1.4, 2, glassDepth, -0.8, 2.5, -16],
+  [1.4, 2, glassDepth, 3.8, 2.5, -16],
+  [glassDepth, 2, 4, 17.6, 2.5, -13.2],
+  [glassDepth, 3, 6.84, 17.6, 2, -2.07],
+  [glassDepth, 2, 1.4, 17.6, 2.5, 5.09],
+  [glassDepth, 2, 5.6, -17.6, 2.5, -12.4],
+  [glassDepth, 3, 12.8, -17.6, 2, 3.2],
 ];
 
 // 地板参数
@@ -174,7 +179,7 @@ const addAllWall = (
     boxGeometry = new BoxGeometry(1, 1, 1);
     assetManager.geometries.set("boxGeometry", boxGeometry);
   }
-  // 创建墙体材质
+  // 墙体材质
   const wallMaterial = new MeshStandardMaterial({
     color: wallColor,
     roughness: 0.85, // 乳胶漆的粗糙度，有轻微漫反射
@@ -182,7 +187,7 @@ const addAllWall = (
     envMapIntensity: 0.3,
   });
   assetManager.materials.set("wallMaterial", wallMaterial);
-  // 创建玻璃材质
+  // 玻璃材质
   const glassMaterial = new MeshStandardMaterial({
     color: 0x87ceeb,
     transparent: true,
@@ -192,6 +197,21 @@ const addAllWall = (
     depthWrite: false, // 透明物体不写深度，避免遮挡后面的透明物体
   });
   assetManager.materials.set("glassMaterial", glassMaterial);
+  // 铝合金包边材质
+  let aluminiumAlloyFrameMaterial = assetManager.materials.get(
+    "aluminiumAlloyFrameMaterial",
+  );
+  if (!aluminiumAlloyFrameMaterial) {
+    aluminiumAlloyFrameMaterial = new MeshStandardMaterial({
+      color: 0xc0c0c8,
+      roughness: 0.3,
+      metalness: 0.9,
+    });
+    assetManager.materials.set(
+      "aluminiumAlloyFrameMaterial",
+      aluminiumAlloyFrameMaterial,
+    );
+  }
 
   // 使用InstancedMesh实例化渲染，提高性能
   const instancedMesh = new InstancedMesh(
@@ -327,12 +347,114 @@ const addGlassWindow = (
 ) => {
   const boxGeometry = assetManager.geometries.get("boxGeometry");
   const glassMaterial = assetManager.materials.get("glassMaterial");
+  const glassWindowGroup = new Group();
+  glassWindowGroup.position.set(x, y, z);
+  scene.add(glassWindowGroup);
   const glassWindow = new Mesh(boxGeometry, glassMaterial);
   pointerControlsIntersetObjectsRef.current?.push(glassWindow);
   glassWindow.scale.set(width, height, depth);
-  glassWindow.position.set(x, y, z);
   glassWindow.name = "玻璃窗";
-  scene.add(glassWindow);
+  glassWindowGroup.add(glassWindow);
+
+  // 添加玻璃窗合金包边
+  const aluminiumAlloyFrameMaterial = assetManager.materials.get(
+    "aluminiumAlloyFrameMaterial",
+  );
+  const topAluminiumAlloyFrame = new Mesh(
+    boxGeometry,
+    aluminiumAlloyFrameMaterial,
+  );
+  const bottomAluminiumAlloyFrame = new Mesh(
+    boxGeometry,
+    aluminiumAlloyFrameMaterial,
+  );
+  const leftAluminiumAlloyFrame = new Mesh(
+    boxGeometry,
+    aluminiumAlloyFrameMaterial,
+  );
+  const rightAluminiumAlloyFrame = new Mesh(
+    boxGeometry,
+    aluminiumAlloyFrameMaterial,
+  );
+  if (width > depth) {
+    // 横向玻璃
+    topAluminiumAlloyFrame.scale.set(
+      width,
+      aluminiumAlloyFrameHeight,
+      aluminiumAlloyFrameDepth,
+    );
+    bottomAluminiumAlloyFrame.scale.set(
+      width,
+      aluminiumAlloyFrameHeight,
+      aluminiumAlloyFrameDepth,
+    );
+    leftAluminiumAlloyFrame.scale.set(
+      aluminiumAlloyFrameHeight,
+      height,
+      aluminiumAlloyFrameDepth,
+    );
+    leftAluminiumAlloyFrame.position.set(
+      (aluminiumAlloyFrameDepth - width) / 2,
+      0,
+      0,
+    );
+    rightAluminiumAlloyFrame.scale.set(
+      aluminiumAlloyFrameHeight,
+      height,
+      aluminiumAlloyFrameDepth,
+    );
+    rightAluminiumAlloyFrame.position.set(
+      (width - aluminiumAlloyFrameDepth) / 2,
+      0,
+      0,
+    );
+  } else {
+    // 竖向玻璃
+    topAluminiumAlloyFrame.scale.set(
+      aluminiumAlloyFrameDepth,
+      aluminiumAlloyFrameHeight,
+      depth,
+    );
+    bottomAluminiumAlloyFrame.scale.set(
+      aluminiumAlloyFrameDepth,
+      aluminiumAlloyFrameHeight,
+      depth,
+    );
+    leftAluminiumAlloyFrame.scale.set(
+      aluminiumAlloyFrameDepth,
+      height,
+      aluminiumAlloyFrameHeight,
+    );
+    leftAluminiumAlloyFrame.position.set(
+      0,
+      0,
+      (depth - aluminiumAlloyFrameHeight) / 2,
+    );
+    rightAluminiumAlloyFrame.scale.set(
+      aluminiumAlloyFrameDepth,
+      height,
+      aluminiumAlloyFrameHeight,
+    );
+    rightAluminiumAlloyFrame.position.set(
+      0,
+      0,
+      (aluminiumAlloyFrameHeight - depth) / 2,
+    );
+  }
+  topAluminiumAlloyFrame.position.set(
+    0,
+    (height - aluminiumAlloyFrameHeight) / 2,
+    0,
+  );
+  bottomAluminiumAlloyFrame.position.set(
+    0,
+    (aluminiumAlloyFrameHeight - height) / 2,
+    0,
+  );
+  glassWindowGroup.add(topAluminiumAlloyFrame);
+  glassWindowGroup.add(bottomAluminiumAlloyFrame);
+  glassWindowGroup.add(leftAluminiumAlloyFrame);
+  glassWindowGroup.add(rightAluminiumAlloyFrame);
 };
 
 // 创建并添加大理石地板
